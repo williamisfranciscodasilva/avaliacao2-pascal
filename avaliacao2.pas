777e173177a2716c6b95012c479dd7d819740454
williamisfranciscodasilva <williamisepaula@gmail.com>
2018-12-20 13:12:52 -0200

    Programa em Pascal para receber as notas das provas e dizer se está aprovado ou não

---
+++ avaliacao2.pas
@@ -0,0 +1,25 @@
+Program avaliacao2;
+
+var
+prova1, prova2, prova3, media: real;
+
+begin
+
+    write('Digite o valor da primeira prova: ');
+    readln(prova1);
+    write('Digite o valor da segunda prova: ');
+    readln(prova2);
+    write('Digite o valor da terceira prova: ');
+    readln(prova3);
+
+    prova1 := (prova1 * 2) / 10;
+    Prova2 := (prova2 * 3) / 10;
+    Prova3 := (prova3 * 5) / 10;
+    media := prova1 + prova2 + prova3;
+
+    if media >= 6 then
+        write('Parabens voce foi APROVADO com media: ',media:6:2)
+    else
+        write('REPROVADO com media: ',media:6:2);
+    readln();
+end.
